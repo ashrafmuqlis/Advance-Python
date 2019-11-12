0d521f81929b883f914473961a6cbeff8f1c572f
ashrafmuqlis <ashrafmuqlis@gmail.com>
2019-11-12 15:42:39 +0800

    Add files via upload

--- logistics regression.py
+++ logistics regression.py
@@ -1,29 +1,38 @@
-
-
-import pandas as pd
-col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
-# load dataset
-pima = pd.read_csv("C:/Users/arun/Desktop/ITRAIN/itrain python/Advanced/codes/DAY 2/diabetes.csv", header=None, names=col_names)
-pima.head()
-#pima=pima.drop(columns=['pedigree'])
-pima.head()
-#Selecting features
-#split dataset in features and target variable
-feature_cols = ['pregnant', 'insulin', 'bmi', 'age','glucose','bp','pedigree']
-X = pima[feature_cols] # Features
-y = pima.label # Target variable
-#Splitting Data
-# split X and y into training and testing sets
+#This data sets consists of 3 different types of irisesâ€™ (Setosa, Versicolour, and Virginica) petal and sepal length, stored in a 150x4 numpy.ndarray
+#import sklearn as sk
+from sklearn import datasets
+#Load Data
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
+X
+Y
+# Split and Randomize Data
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
-# import the class
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=33)
+X_train
+X_test
+
+
+#Step 2
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+clf=LogisticRegression()
 
-# instantiate the model (using the default parameters)
-logreg = LogisticRegression()
+#Step 3
+clf.fit(X_train,Y_train)
 
-# fit the model with data
-logreg.fit(X_train,y_train)
+#Step 4
+pred = clf.predict(X_test)
 
-#
-y_pred=logreg.predict(X_test)
+#Step 5 
+print ("Logistic Regression accuracy score : ", accuracy_score(Y_test,pred))
+    
+#VISUALIZATION
+#conda install -c districtdatalabs yellowbrick
+from yellowbrick.classifier import ClassificationReport
+# Instantiate the classification model and visualizer
+visualizer = ClassificationReport(clf, classes=[0,1,2])
+visualizer.fit(X_train, Y_train) # Fit the training data to the visualizer
+visualizer.score(X_test, Y_test) # Evaluate the model on the test data
+g = visualizer.poof() # Draw/show/poof the data
